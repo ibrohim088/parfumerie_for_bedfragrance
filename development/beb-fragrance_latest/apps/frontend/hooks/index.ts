943badea16213e6aacc402a43cfc655f867e155f
iborhim008 <sunnat5809@gmail.com>
2026-06-22 12:25:10 +0500

    update frontend hooks index.ts

--- development/beb-fragrance_latest/apps/frontend/hooks/index.ts
+++ development/beb-fragrance_latest/apps/frontend/hooks/index.ts
@@ -3,18 +3,18 @@ export { useProduct } from './useProduct';
 export { useBrands } from './useBrands';
 export { useDecants } from './useDecants';
 export { useBlog, useBlogPost } from './useBlog';
-import useOrders from './useOrders';
-import useOrder from './useOrder';
-import useCart from './useCart';
-import useAuth from './useAuth';
-import useTheme from './useTheme';
-import useAddresses from './useAddresses';
-import useNotifications from './useNotifications';
-import useScentProfile from './useScentProfile';
-import usePayment from './usePayment';
-import useLocalStorage from './useLocalStorage';
-import useMediaQuery from './useMediaQuery';
-import useDebounce from './useDebounce';
+export { useOrders } from './useOrders';
+export { useOrder } from './useOrder';
+export { useCart } from './useCart';
+export { useAuth } from './useAuth';
+export { useTheme } from './useTheme';
+export { useAddresses } from './useAddresses';
+export { useNotifications } from './useNotifications';
+export { useScentProfile } from './useScentProfile';
+export { usePayment } from './usePayment';
+export { useLocalStorage } from './useLocalStorage';
+export { useMediaQuery } from './useMediaQuery';
+export { useDebounce } from './useDebounce';
 
 export {
   useOrders,
@@ -30,3 +30,23 @@ export {
   useMediaQuery,
   useDebounce,
 };
+
+/*
+  export { useProducts } from './useProducts';
+  export { useProduct } from './useProduct';
+  export { useBrands } from './useBrands';
+  export { useDecants } from './useDecants';
+  export { useBlog, useBlogPost } from './useBlog';
+  import useOrders from './useOrders';
+  import useOrder from './useOrder';
+  import useCart from './useCart';
+  import useAuth from './useAuth';
+  import useTheme from './useTheme';
+  import useAddresses from './useAddresses';
+  import useNotifications from './useNotifications';
+  import useScentProfile from './useScentProfile';
+  import usePayment from './usePayment';
+  import useLocalStorage from './useLocalStorage';
+  import useMediaQuery from './useMediaQuery';
+  import useDebounce from './useDebounce';
+*/
